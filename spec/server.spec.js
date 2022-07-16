b643e6f1afa0fd9f21b4e7c37d3f4c2a1d4dc26b
Shubhanshu Jain <shubhanshu.jain@girnanrsoft.co.in>
2022-07-16 21:38:39 +0530

    Implemented test cases for response

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,5 +1,25 @@
+var request = require('request');
+
 describe('calc', ()=>{
   it('should multiply', ()=>{
     expect(2*2).toBe(4)
+  })
+})
+
+describe('get messages', () => {
+  it('should return 200 OK', (done)=>{
+    request.get('http://localhost:3000/messages', (error, response)=>{
+      console.log(response.statusCode);
+      expect(response.statusCode).toEqual(200)
+      done();
+    })
+  })
+
+  it('List should not be Empty', (done)=>{
+    request.get('http://localhost:3000/messages', (error, response)=>{
+      console.log(JSON.parse(response.body));
+      expect(JSON.parse(response.body).length).toBeGreaterThan(0);
+      done()
+    })
   })
 })
